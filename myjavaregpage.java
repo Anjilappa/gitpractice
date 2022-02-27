bf7ae93268917dbcdd5c42976d9ae610df5300a2
unknown <ysanjilappa@gmail.com>
2022-02-27 20:03:42 +0530

    my 100% work completed

--- myjavaregpage.java
+++ myjavaregpage.java
@@ -1,2 +1,10 @@
 this is my javareg page
+<<<<<<< Updated upstream
 this is my managers work completed
+=======
+1st day of java lines of code
+2nd day of java lines of code
+3rd day of java lines of code
+4th day of java lines of code 90% of task completed
+5th day of java lines of code
+>>>>>>> Stashed changes
